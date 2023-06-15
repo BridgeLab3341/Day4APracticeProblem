04f0fa702c72721bdad40411bab26e3730b3ab6a
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-15 22:22:57 +0530

    [somu]Add:NLog Implimentation On Demo Project and Info about Project.

---
+++ Day4APracticeProblem/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Day4APracticeProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DemoExample demo = new DemoExample();
+            demo.Sum(5, 9);
+        }
+    }
+}
